d4f55ffff2646d14c5b2ffe3ae3227d1277ea58f
skeuker <stefan.keuker@gmail.com>
2019-05-15 19:29:51 +0000

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("pnp.itservicescost.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+sap.ui.define(["sap/ui/generic/app/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("pnp.itservicescost.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
